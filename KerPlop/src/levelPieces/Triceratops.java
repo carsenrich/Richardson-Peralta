60106bfa3308c8afd554c03f651f2295ca59d8cd
carsenrich <carmela_richardson@mines.edu>
2026-09-23 12:03:27 -0600

    Add KerPlop game pieces

---
+++ KerPlop/src/levelPieces/Triceratops.java
@@ -0,0 +1,14 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Triceratops extends GamePiece {
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

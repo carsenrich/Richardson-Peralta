c26699eb19bd18179f1db82df6d1751e2e49b7df
carsenrich <carmela_richardson@mines.edu>
2026-09-23 12:02:28 -0600

    Add KerPlop game pieces

---
+++ KerPlop/src/levelPieces/Stegosaurus.java
@@ -0,0 +1,14 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Stegosaurus extends GamePiece {
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

aff378c6ddfd54b35c16f8ada6ff0cfc4c2d271c
Frank Peralta <frankmlperalta@gmail.com>
2026-09-23 11:37:44 -0600

    Updated LevelSetup and Mountain

--- KerPlop/src/levelPieces/LevelSetup.java
+++ KerPlop/src/levelPieces/LevelSetup.java
@@ -27,18 +27,44 @@ public class LevelSetup {
 		
 		if (levelNum == 1) {
 			
-			playerStartLoc =10 ; 
+			playerStartLoc = 10 ; 
+			
+			Prize prize = new Prize(5);
+			gameBoard[prize.getLocation()] = prize;
+			interactingPieces.add(prize);
 					
 			TRex trex = new TRex(18); 
-			
-			gameBoard[trex.getLocation()]= trex;
+			gameBoard[trex.getLocation()] = trex;
 			movingPieces.add(trex);
 			interactingPieces.add(trex);
 			
+			Mountain mountain = new Mountain(3);
+			gameBoard[mountain.getLocation()] = mountain;
+			
+			Brontosaurus bronto = new Brontosaurus(4);
+			gameBoard[bronto.getLocation()] = bronto;
+			interactingPieces.add(bronto);
 			
 			
 		}else if (levelNum == 2) {
 			
+			playerStartLoc = 7 ; 
+			
+			Prize prize = new Prize(12);
+			gameBoard[prize.getLocation()] = prize;
+			interactingPieces.add(prize);
+					
+			TRex trex = new TRex(20); 
+			gameBoard[trex.getLocation()] = trex;
+			movingPieces.add(trex);
+			interactingPieces.add(trex);
+			
+			Mountain mountain = new Mountain(9);
+			gameBoard[mountain.getLocation()] = mountain;
+			
+			Brontosaurus bronto = new Brontosaurus(18);
+			gameBoard[bronto.getLocation()] = bronto;
+			interactingPieces.add(bronto);
 			
 		}
 		
--- KerPlop/src/levelPieces/Mountain.java
+++ KerPlop/src/levelPieces/Mountain.java
@@ -1,5 +1,36 @@
+/*
+ * Class: Mountain class, implements Drawable. This is the one item that does not interact with the player
+ * Authors: Carmela Richardson and Frank Peralta
+ * Date: 9/6/26
+ * 
+ */
+
 package levelPieces;
 
-public class Mountain {
+import gameEngine.Drawable;
 
+
+public class Mountain implements Drawable{
+	
+	private char symbol = 'M';
+	private int location;
+	
+	public Mountain(int location) {
+		this.location = location;
+	}
+
+	@Override
+	public void draw() {
+		System.out.println(symbol);
+		
+	}
+
+	public int getLocation() {
+		return location;
+	}
+	
+	
+	
 }
+
+	

42a232b6d15a5ff9780600a061dab9f10c6e6727
carsenrich <carmela_richardson@mines.edu>
2026-09-23 12:42:01 -0600

    Added test cases

---
+++ KerPlop/src/tests/TRexTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TRexTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

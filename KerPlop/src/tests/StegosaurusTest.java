e7bffa1b84cfa0adb22950ab107a80f31f4407e2
carsenrich <carmela_richardson@mines.edu>
2026-09-23 20:19:02 -0600

    Finished test cases for Stegosaurus and Triceratops.

--- KerPlop/src/tests/StegosaurusTest.java
+++ KerPlop/src/tests/StegosaurusTest.java
@@ -4,11 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import gameEngine.GameEngine;
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+import levelPieces.Stegosaurus;
+
 class StegosaurusTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testStegosaurus() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Stegosaurus stego = new Stegosaurus(12);
+		gameBoard[12] = stego;
+		
+		// Hit points only if player on same space
+		assertEquals(InteractionResult.HIT, stego.interact(gameBoard, 12));
+		
+		// These loops ensure no interaction if not on same space
+		for (int i=0; i<12; i++)
+			assertEquals(InteractionResult.NONE, stego.interact(gameBoard, i));
+		for (int i=13; i<GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, stego.interact(gameBoard, i));
+		
 	}
 
 }
--- KerPlop/src/tests/TriceratopsTest.java
+++ KerPlop/src/tests/TriceratopsTest.java
@@ -4,12 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import gameEngine.Drawable;
+import levelPieces.Triceratops;
+
 class TriceratopsTest {
 
 	@Test
 	public void testTriceratops() {
-		gameBoard = new Drawable[GameEngine]
-		fail("Not yet implemented");
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Triceratops tricera = new Triceratops(12);
+		gameBoard[12] = tricera;
+		
+		// Hit points only if player is on same or adjacent spot
+		assertEquals(InteractionResult.HIT, tricera.interact(gameBoard, 11));
+		assertEquals(InteractionResult.HIT, tricera.interact(gameBoard, 12));
+		assertEquals(InteractionResult.HIT, tricera.interact(gameBoard, 13));
+		
+		// These loops ensure no interaction if not on same space
+		for (int i=0; i<11; i++)
+			assertEquals(InteractionResult.NONE, tricera.interact(gameBoard, i));
+		for (int i=14; i<GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, tricera.interact(gameBoard, i));
 	}
 
 }
